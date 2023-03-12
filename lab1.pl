6c86b28da08439c8079bce1381ab3891a9068982
Amir <127651398+DanishAlimughal@users.noreply.github.com>
2023-03-12 00:17:52 -0800

    Create lab file work

---
+++ lab1.pl
@@ -0,0 +1,6 @@
+male(jaree).
+male(umair).
+male(arbaz).
+male(amir).
+female(aelia).
+female(nabia).
---
+++ lab2.pl
@@ -0,0 +1,12 @@
+father(arsalan,saima).
+father(arsalan,saima).
+father(arsalan,saima).
+friend(ali,farhan).
+driver(farhan,car).
+likes(ali,iceream).
+
+facts(properties).
+woman(saima).
+man(farhan).
+walk(farhan).
+happy(ali).
